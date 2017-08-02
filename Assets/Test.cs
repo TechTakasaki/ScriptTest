c179e95873d669f9101b20f2d4b05f7888e66dfe
布施和隆 <firebom.sns@gmail.com>
2017-08-02 22:39:59 +0900

    よろしくお願いします。

---
+++ Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour{
+        void Start(){
+                int[] array = new int[5];
+
+                array[0] = 40;
+                array[1] = 50;
+                array[2] = 56;
+                array[3] = 13;
+                array[4] = 7;
+
+                for (int i = 4; i >=0; i--){
+                        Debug.Log(array [i]);
+                }
+        }
+
+        // Update is called once per frame
+        void Update(){
+
+        }
+}
